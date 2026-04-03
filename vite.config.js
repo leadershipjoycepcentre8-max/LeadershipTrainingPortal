85ae1fbcaaa7bb321cbc01167c4fbd8a009ce038
leadershipjoycepcentre8-max <leadershipjoycepcentre8@gmail.com>
2026-04-03 13:32:21 +0000

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,21 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
-
   plugins: [react()],
+
+  base: "/LeadershipTrainingPortal/",
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
 
-  root: path.resolve(__dirname, "client"),
-
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
-
+});
   server: {
     fs: {
       strict: true,
